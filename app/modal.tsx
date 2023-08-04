70da69346eb4f08aaade9634bfb7fd43195cb7ba
DevJonathanMendes <jonathanbarreiro88@gmail.com>
2023-08-04 12:53:57 -0300

    Style: removed <View />

--- app/modal.tsx
+++ app/modal.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { View } from '../components/base/Themed';
 import { BoldText } from '../components/base/StyledText';
 import { ViewFlexCenter } from '../components/base/StyledView';
 
@@ -7,10 +6,6 @@ export default function ModalScreen() {
 	return (
 		<ViewFlexCenter>
 			<BoldText>Não implementado.</BoldText>
-			<View
-				style={{ marginVertical: 30, height: 1, width: '80%' }}
-				darkColor='rgba(255,255,255,0.1)'
-			/>
 		</ViewFlexCenter>
 	);
 }
